0a3cd21b51c06ad3e8aa816cd3ad9e5db2e16fff
Prashanth Krishnan <85628036+pkrish012@users.noreply.github.com>
2026-02-20 14:57:14 -0500

    Update tests for OrderBug with expected exceptions

--- src/test/java/com/demo/OrderBugTest.java
+++ src/test/java/com/demo/OrderBugTest.java
@@ -4,9 +4,16 @@ import org.junit.Test;
 
 public class OrderBugTest {
 
-    @Test
+    @Test(expected = IllegalStateException.class)
     public void testOrderDependency() {
         OrderBug bug = new OrderBug();
-        bug.stepTwo(); // <- this should fail
+        bug.stepTwo(); // <- should fail
+    }
+
+    @Test
+    public void testCorrectOrder() {
+        OrderBug bug = new OrderBug();
+        bug.stepOne();
+        bug.stepTwo(); // <- should succeed
     }
 }
